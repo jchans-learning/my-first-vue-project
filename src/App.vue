8138cc1654b68290fcc9d4567f19ca2dca40646e
jchans <hanschiang86@gmail.com>
2021-11-17 00:45:50 +0800

    Add VueMemo Component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <VueMemo />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import VueMemo from "./components/VueMemo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    VueMemo,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/VueMemo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="vue-memo">
+    <ul>
+      <li>Windows 環境下建立專案。</li>
+      <li>測試用 PowerShell 的 Script "deploy.ps1" deploy 成功。</li>
+      <li>測試用 cmd 的 batch "deploy.bat" deploy 。（測試中）</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VueMemo",
+};
+</script>
+
+<style>
+.vue-memo {
+  margin-top: 2rem;
+  text-align: left;
+}
+</style>
