dd6ed97072d43e7b996fb0b9ced6986ec5aa5562
jchans <hanschiang86@gmail.com>
2021-11-17 00:53:49 +0800

    Test deploy with new component VueMemo

--- src/components/VueMemo.vue
+++ src/components/VueMemo.vue
@@ -3,7 +3,10 @@
     <ul>
       <li>Windows 環境下建立專案。</li>
       <li>測試用 PowerShell 的 Script "deploy.ps1" deploy 成功。</li>
-      <li>測試用 cmd 的 batch "deploy.bat" deploy 。（測試中）</li>
+      <li>
+        測試用 cmd 的 batch "deploy.bat" deploy 。測試失敗（ 2021-11-17 00:50
+        ），待確認原因。
+      </li>
     </ul>
   </div>
 </template>
